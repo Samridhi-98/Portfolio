4388467956d2eecee47c08a94be3f2bf8aa4fc74
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-02-16 20:57:54 +0530

    dark-mode implemention 0.1

--- src/components/App.js
+++ src/components/App.js
@@ -9,55 +9,56 @@ import About from "./routes/About";
 //REACT SIDEBAR
 import Sidebar from "react-sidebar";
 import SidebarContent from "./routes/SidebarContent";
+
 // IMAGE
-// import sidebarbg2 from "../images/sidebarbg2.jpg";
-import sidebarbg from "../images/sidebarbg.jpg";
+// import sidebarbg from "../images/sidebarbg.jpg";
+
+import { styles } from "./sidebar-style/style"
 
 const mql = window.matchMedia(`(min-width: 800px)`);
-let styles = {
-  root: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    overflow: "hidden"
-  },
-  sidebar: {
-    width: 500,
-    height: "100%",
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.6)),url(${sidebarbg})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  },
-  content: {
-    padding: "16px",
-    height: "100%",
-    backgroundColor: "white"
-  },
-  overlay: {
-    zIndex: 1,
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    opacity: 0,
-    visibility: "hidden",
-    transition: "opacity .3s ease-out, visibility .3s ease-out",
-    backgroundColor: "rgba(0,0,0,.3)"
-  },
-  dragHandle: {
-    zIndex: 1,
-    position: "fixed",
-    top: 0,
-    bottom: 0
-  }
-}
+// let styles = {
+//   root: {
+//     position: "absolute",
+//     top: 0,
+//     left: 0,
+//     right: 0,
+//     bottom: 0,
+//     overflow: "hidden"
+//   },
+//   sidebar: {
+//     width: 500,
+//     height: "100%",
+//     backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.6)),url(${sidebarbg})`,
+//     backgroundSize: "cover",
+//     backgroundPosition: "center",
+//   },
+//   content: {
+//     padding: "16px",
+//     height: "100%",
+//     backgroundColor: "white"
+//   },
+//   overlay: {
+//     zIndex: 1,
+//     position: "fixed",
+//     top: 0,
+//     left: 0,
+//     right: 0,
+//     bottom: 0,
+//     opacity: 0,
+//     visibility: "hidden",
+//     transition: "opacity .3s ease-out, visibility .3s ease-out",
+//     backgroundColor: "rgba(0,0,0,.3)"
+//   },
+//   dragHandle: {
+//     zIndex: 1,
+//     position: "fixed",
+//     top: 0,
+//     bottom: 0
+//   }
+// }
 
 
 class App extends React.Component {
-  // Sidebar content
   constructor(props) {
     super(props);
     this.state = {
@@ -68,9 +69,9 @@ class App extends React.Component {
     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
     this.setTheme = this.setTheme.bind(this);
   }
-  // componentWillMount() {
-  //   mql.addListener(this.mediaQueryChanged);
-  // }
+  componentWillMount() {
+    mql.addListener(this.mediaQueryChanged);
+  }
 
   componentWillUnmount() {
     this.state.mql.removeListener(this.mediaQueryChanged);
--- src/index.js
+++ src/index.js
@@ -3,15 +3,5 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-window.onload = function () {
-    if (localStorage.getItem("theme")) {
-        document.getElementById("body").className =
-            localStorage.getItem("theme") || "paper";
-        console.log("Preferred theme: ", localStorage.getItem("theme"));
-    } else {
-        console.log("Default theme: ", document.getElementById("body").className);
-    }
-};
-
 
 ReactDOM.render(<App />, document.querySelector("#root"));
